15b2d6cfbdef9010cec19e021864b31bca426522
orangepek0e <georgejohnston13@hotmail.com>
2015-11-14 11:36:58 -0400

    js moved to external to prevent conflicts

--- js/twitterAuth.js
+++ js/twitterAuth.js
@@ -1,3 +1,22 @@
 /**
  * Created by orangepek0e on 15-11-14.
  */
+<script>
+var dataRef = new Firebase('https://redspace-hackathon.firebaseio.com/');
+var userName = '';
+var userScore = '';
+var authModal = $('#auth-modal').modal({show: false});
+
+function logIn(){
+    console.log("you are trying to log in");
+
+    dataRef.authWithOAuthPopup("twitter", function(error, authData) {
+        if (error) {
+            console.log("Login Failed!", error);
+        } else {
+            console.log("Authenticated successfully with payload:", authData);
+        }
+    });
+}
+
+</script>
