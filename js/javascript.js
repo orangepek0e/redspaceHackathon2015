df2f175cc38aedb28a763879e7a8f7c636db6bf1
pointcha <info@pointcha.ca>
2015-11-14 18:48:30 -0400

    swap

--- js/javascript.js
+++ js/javascript.js
@@ -50,6 +50,7 @@ function logIn(provider) {
                 }
                 $('#loginScreen').toggle();
                 $('#notificationScreen').toggle();
+                challengePicker()
             });
         }
     });
@@ -103,22 +104,22 @@ function challengePicker() {
     var challenge = Math.floor((Math.random() * 6) + 1);
     switch(challenge) {
         case 1:
-            $('.page-wrapper').addClass("meme1");
+            $('#notificationScreen').addClass("meme1");
             break;
         case 2:
-            $('.page-wrapper').addClass("meme2");
+            $('#notificationScreen').addClass("meme2");
             break;
         case 3:
-            $('.page-wrapper').addClass("meme3");
+            $('#notificationScreen').addClass("meme3");
             break;
         case 4:
-            $('.page-wrapper').addClass("meme4");
+            $('#notificationScreen').addClass("meme4");
             break;
         case 5:
-            $('.page-wrapper').addClass("meme5");
+            $('#notificationScreen').addClass("meme5");
             break;
         case 6:
-            $('.page-wrapper').addClass("meme6");
+            $('#notificationScreen').addClass("meme6");
             break;
 
     }
