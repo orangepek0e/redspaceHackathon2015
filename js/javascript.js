30b752acfcd7fb641b85e277a1ed441a9486c859
orangepek0e <georgejohnston13@hotmail.com>
2015-11-14 13:07:08 -0400

    timer added

--- js/javascript.js
+++ js/javascript.js
@@ -11,17 +11,6 @@ if (authData){
     console.log('user is already logged in');
 }
 
-//function logIn(){
-//    console.log("you are trying to log in");
-//
-//    dataRef.authWithOAuthPopup("twitter", function(error, authData) {
-//        if (error) {
-//            console.log("Login Failed!", error);
-//        } else {
-//            console.log("Authenticated successfully with payload:", authData);
-//        }
-//    });
-//}
 
 function logIn(provider) {
     myDataRef.authWithOAuthPopup(provider, function(error, authData) {
@@ -58,3 +47,36 @@ function setUserNodeId() {
     });
 }
 
+function startTimer(duration, display) {
+    var start = Date.now(),
+        diff,
+        minutes,
+        seconds;
+    function timer() {
+
+        diff = duration - (((Date.now() - start) / 1000) | 0);
+
+
+        minutes = (diff / 60) | 0;
+        seconds = (diff % 60) | 0;
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (diff <= 0) {
+
+            start = Date.now() + 1000;
+        }
+    };
+
+    timer();
+    setInterval(timer, 1000);
+}
+
+window.onload = function () {
+    var fiveMinutes = 60 * 5,
+        display = document.querySelector('#time');
+    startTimer(fiveMinutes, display);
+};
